cf549b9919bdf3f39e85238e892a20ab20e839b0
StefanieSchmidiger <stefanie.schmidiger@stud.hslu.ch>
2016-04-19 15:42:07 +0200

    TwinCat Program works

---
+++ HMT/Demoprogramm_mitSensoren/demoapp/MultiByte/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+..\stack\stackmultibyte.cpp
+
+..\stack\stacksinglebyte.cpp
+
+..\stack\stacktransparent.cpp
+
+demoapp.cpp
+
+i2c_master.c
+
+main.cpp
+
